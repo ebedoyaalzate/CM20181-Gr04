b62d90155cec9569399e94c698ee8ffb057ae985
Esteban <esteban.bedoyaa@udea,edu.co>
2018-04-16 21:23:21 -0500

    casi listo

--- Lab1/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20181/lab1/MainActivity.java
+++ Lab1/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20181/lab1/MainActivity.java
@@ -1,23 +1,46 @@
 package co.edu.udea.compumovil.gr04_20181.lab1;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton bebidas,comidas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         //Para Creae los Actions bar a mi gusto
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        getSupportActionBar().setCustomView(R.layout.abs_layout);
+         getSupportActionBar().setCustomView(R.layout.abs_layout);
 
-        //Flecha para regresar a la anterior activity
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        bebidas= (FloatingActionButton) findViewById(R.id.bebidas);
+        comidas= (FloatingActionButton) findViewById(R.id.comidas);
+
+        bebidas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent bebidas = new Intent(MainActivity.this,bebida.class);
+                startActivity(bebidas);
+            }
+        });
+        comidas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent comidas = new Intent(MainActivity.this,comida.class);
+                startActivity(comidas);
+            }
+        });
     }
 
 
